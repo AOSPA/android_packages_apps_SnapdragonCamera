af98d49aae92c9b2f2a76dcd59147be82e3b970c
Sultanxda <sultanxda@gmail.com>
2015-10-04 00:20:01 -0700

    SnapdragonCamera: Always lock AE and AWB when auto-focus is used
    Lock the auto exposure (AE) and auto white balance (AWB) when tap-to-focus
    is used.
    
    Change-Id: I9d8e9b4928516e8b2bad7be958e5c4849db19ab6
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- src/com/android/camera/FocusOverlayManager.java
+++ src/com/android/camera/FocusOverlayManager.java
@@ -294,6 +294,9 @@ public class FocusOverlayManager {
             // sound.
             if (focused) {
                 mState = STATE_SUCCESS;
+                // Lock exposure and white balance
+                setAeAwbLock(true);
+                mListener.setFocusParameters();
             } else {
                 mState = STATE_FAIL;
             }
@@ -305,6 +308,9 @@ public class FocusOverlayManager {
             // take the picture now.
             if (focused) {
                 mState = STATE_SUCCESS;
+                // Lock exposure and white balance
+                setAeAwbLock(true);
+                mListener.setFocusParameters();
             } else {
                 mState = STATE_FAIL;
             }
@@ -459,6 +465,7 @@ public class FocusOverlayManager {
         // Otherwise, focus mode stays at auto and the tap area passed to the
         // driver is not reset.
         resetTouchFocus();
+        setAeAwbLock(false);
         mListener.cancelAutoFocus();
         mUI.resumeFaceDetection();
         mState = STATE_IDLE;
