ab603ea3ec71a5eeb293843efb1f269a06f89e3c
Thecrazyskull <anaskarbila@gmail.com>
2017-01-20 17:46:26 -0600

    [NEVER MERGE!] Camera: Load Oneplus proprietary libs
    Change-Id: Ia394b2390b3eefde45176890f7c38ac32306692d

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -263,6 +263,8 @@ public class PhotoModule
     private byte[] mLastJpegData;
     private int mLastJpegOrientation = 0;
 
+    private boolean mLibraryLoaded = false;
+
     private static Context mApplicationContext = null;
 
     private Runnable mDoSnapRunnable = new Runnable() {
@@ -626,11 +628,18 @@ public class PhotoModule
         if (mCameraState == SNAPSHOT_IN_PROGRESS) {
             return;
         }
+        loadNativeLibrary();
         setCameraState(IDLE);
         mFocusManager.onPreviewStarted();
         startFaceDetection();
         locationFirstRun();
         mUI.enableShutter(true);
+    }
+
+    private void loadNativeLibrary() {
+        if (mLibraryLoaded) return;
+        System.loadLibrary("opcameralib");
+        mLibraryLoaded = true;
     }
 
     // Prompt the user to pick to record location for the very first run of
