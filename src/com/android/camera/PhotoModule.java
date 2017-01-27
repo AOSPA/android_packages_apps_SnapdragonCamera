ca392b37c1c2759ad8b17dd50865ba0283093191
Thecrazyskull <anaskarbila@gmail.com>
2017-01-27 12:52:40 -0600

    PhotoModule: silence logspam
    Change-Id: I4e9bba129fdd284a602ef227cd2d4a04cfd3ffad

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -5081,7 +5081,7 @@ class GraphView extends View {
     private float scaled;
     private static final int STATS_SIZE = 256;
     private static final String TAG = "GraphView";
-
+    private static final boolean DEBUG = false;
 
     public GraphView(Context context, AttributeSet attrs) {
         super(context,attrs);
@@ -5100,9 +5100,9 @@ class GraphView extends View {
     }
     @Override
     protected void onDraw(Canvas canvas) {
-        Log.v(TAG, "in Camera.java ondraw");
+        if (DEBUG) Log.v(TAG, "in Camera.java ondraw");
         if(mPhotoModule == null || !mPhotoModule.mHiston ) {
-            Log.e(TAG, "returning as histogram is off ");
+            if (DEBUG) Log.e(TAG, "returning as histogram is off ");
             return;
         }
 
