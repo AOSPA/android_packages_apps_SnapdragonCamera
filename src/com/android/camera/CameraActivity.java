1a5dd0bd86683da5b780c4a12686c56ce676eb17
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-01-28 14:56:35 +0100

    SnapdragonCamera: detect and use Camera2 if available
    Also add overlay option to enable support for Camera2 to retain current behaviour.
    
    Change-Id: I20939e33f4bb687e4abea11bbcdb9bf246b156e4
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -196,11 +196,15 @@ public class CameraActivity extends Activity
     private static boolean PIE_MENU_ENABLED = false;
     private boolean mDeveloperMenuEnabled = false;
 
+    private boolean mCamera2supported = false;
+    private boolean mCamera2enabled = false;
+
     /** This data adapter is used by FilmStripView. */
     private LocalDataAdapter mDataAdapter;
     /** This data adapter represents the real local camera data. */
     private LocalDataAdapter mWrappedDataAdapter;
 
+    private Context mContext;
     private PanoramaStitchingManager mPanoramaManager;
     private PlaceholderManager mPlaceholderManager;
     private int mCurrentModuleIndex;
@@ -1471,6 +1475,7 @@ public class CameraActivity extends Activity
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
+        mContext = getApplicationContext();
 
         final boolean useHal3 = getResources().getBoolean(R.bool.config_use_hal3);
         CameraManagerFactory.getAndroidCameraManager().setHal3(useHal3);
@@ -1553,8 +1558,14 @@ public class CameraActivity extends Activity
             }
         }
 
-        boolean cam2on = PersistUtil.getCamera2Mode();
-        if (cam2on && (moduleIndex == ModuleSwitcher.PHOTO_MODULE_INDEX ||
+        // Check if the device supports Camera API 2
+        mCamera2supported = CameraUtil.isCamera2Supported(mContext);
+        Log.d(TAG, "Camera API 2 supported: " + mCamera2supported);
+
+        mCamera2enabled = mCamera2supported && mContext.getResources().getBoolean(R.bool.support_camera_api_v2);
+        Log.d(TAG, "Camera API 2 enabled: " + mCamera2enabled);
+
+        if (mCamera2enabled && (moduleIndex == ModuleSwitcher.PHOTO_MODULE_INDEX ||
                 moduleIndex == ModuleSwitcher.VIDEO_MODULE_INDEX))
             moduleIndex = ModuleSwitcher.CAPTURE_MODULE_INDEX;
 
@@ -1668,9 +1679,9 @@ public class CameraActivity extends Activity
         }
 
         if (!Glide.isSetup()) {
-            Glide.setup(new GlideBuilder(getApplicationContext())
+            Glide.setup(new GlideBuilder(mContext)
                     .setResizeService(new FifoPriorityThreadPoolExecutor(2)));
-            Glide.get(getApplicationContext()).setMemoryCategory(MemoryCategory.HIGH);
+            Glide.get(mContext).setMemoryCategory(MemoryCategory.HIGH);
         }
     }
 
@@ -2094,9 +2105,8 @@ public class CameraActivity extends Activity
 
     @Override
     public void onModuleSelected(int moduleIndex) {
-        boolean cam2on = PersistUtil.getCamera2Mode();
         mForceReleaseCamera = moduleIndex == ModuleSwitcher.CAPTURE_MODULE_INDEX ||
-                (cam2on && moduleIndex == ModuleSwitcher.PHOTO_MODULE_INDEX);
+                (mCamera2enabled && moduleIndex == ModuleSwitcher.PHOTO_MODULE_INDEX);
         if (mForceReleaseCamera) {
             moduleIndex = ModuleSwitcher.CAPTURE_MODULE_INDEX;
         }
