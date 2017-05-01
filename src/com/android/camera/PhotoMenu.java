49291d93c3e32b110be5e48bb5d985da23dd8a4b
Alex Naidis <alex.naidis@linux.com>
2017-05-01 23:50:36 +0200

    SnapdragonCamera: Add picture format options to the menu
    These contain useful options as saving images in RAW.
    
    Change-Id: Ie3ec980ac9d4604d34e30d4edb74d6dc862045a6
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- src/com/android/camera/PhotoMenu.java
+++ src/com/android/camera/PhotoMenu.java
@@ -182,6 +182,7 @@ public class PhotoMenu extends MenuController
                 CameraSettings.KEY_FLASH_MODE,
                 CameraSettings.KEY_RECORD_LOCATION,
                 CameraSettings.KEY_PICTURE_SIZE,
+                CameraSettings.KEY_PICTURE_FORMAT,
                 CameraSettings.KEY_JPEG_QUALITY,
                 CameraSettings.KEY_TIMER,
                 CameraSettings.KEY_CAMERA_SAVEPATH,
